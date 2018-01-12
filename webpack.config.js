fa5f7398d4823c0ae039b71ce843a06e54fabe14
Stanislav Maruniak <stanislav.maruniak@kindgeek.com>
2018-01-12 16:07:33 +0200

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,26 @@ module.exports = {
         './entry.js',
     ],
     output: {
-        path: path.join(__dirname, 'www'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            include: path.join(__dirname, 'src'),
-            query: {
-                presets: [ 'es2015', 'react', 'react-hmre' ]
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'src'),
+                query: {
+                    presets: [ 'es2015', 'react', 'react-hmre' ]
+                }
+            },
+            {
+                test:/\.(s*)css$/,
+                loader: "style-loader!css-loader",
+                include: path.join(__dirname, 'src'),
             }
-        }]
+        ]
     },
     resolve: {
         modules: [
